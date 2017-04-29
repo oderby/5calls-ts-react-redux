99a9b63de2b1a641eae3fa828d50882dcabd0371
Craig Doremus <cdoremus@gmail.com>
2017-04-28 22:47:13 -0400

    Added IApplicationState

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,10 +2,15 @@ import { routerReducer as routing } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import IssueListReducer from './reducer_issues';
 import ActiveIssue from './reducer_active_issue';
+import { IIssue } from '../model';
 
+export interface IApplicationState {
+  routing: any;
+  issues: IIssue[];
+  activeIssue: IIssue;
+}
 
-
-const reducers = combineReducers<any>({
+const reducers = combineReducers<IApplicationState>({
   routing,
   issues: IssueListReducer,
   activeIssue: ActiveIssue
