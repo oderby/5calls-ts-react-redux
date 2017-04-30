d3b050f91ac26852291d2c6dd85bec9728c45722
Craig Doremus <cdoremus@gmail.com>
2017-04-29 20:21:43 -0400

    Specified RouterState typing

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,11 @@
-import { routerReducer as routing } from 'react-router-redux';
+import { routerReducer as routing, RouterState } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import issueListReducer from './reducer_issues';
 import activeIssueReducer from './reducer_active_issue';
 import { IIssue } from '../model';
 
 export interface IApplicationState {
-  routing: any;
+  routing: RouterState;
   issues: IIssue[];
   activeIssue: IIssue;
 }
