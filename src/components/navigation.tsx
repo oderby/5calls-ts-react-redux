f7af9918b4e42edf99ef69c57352ad3fd160f4aa
Craig Doremus <cdoremus@gmail.com>
2017-05-02 22:00:10 -0400

    Renamed glamorous components with a Style suffix

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 import glamorous from 'glamorous';
 
-const NavBorder = glamorous.div({
+const NavBorderStyle = glamorous.div({
   position: 'fixed',
   border: 'auto 0',
   padding: '2px 80px',
   width: '80%'
 });
 
-const NavSpan = glamorous.span({
+const NavSpanStyle = glamorous.span({
   padding: '4px'
 });
 
@@ -18,14 +18,14 @@ export interface IProps {}
 const Navigation: React.SFC<IProps> = (props: IProps) => (
     <div>
       <hr style={{border: '2px solid darkgray', width: '90%'}}/>
-      <NavBorder>
-        <NavSpan>
+      <NavBorderStyle>
+        <NavSpanStyle>
           <NavLink className="navlink" to="/" activeStyle={{color: 'red'}} exact={true}>Home</NavLink>
-        </NavSpan>
-        <NavSpan>
+        </NavSpanStyle>
+        <NavSpanStyle>
           <NavLink className="navlink" to="/about" activeStyle={{color: 'red'}}>About</NavLink>
-        </NavSpan>
-      </NavBorder>
+        </NavSpanStyle>
+      </NavBorderStyle>
     </div>
 );
 
